513a5defa3ec2885ff4d02f62b189fcf7faf6f4d
Sergey Aganezov jr <sergeyaganezovjr@gmail.com>
2014-04-20 12:03:17 -0400

    adding information about genome, that particular chromosome is located on added method for synteny block creation (_create_sb method needs to be rewritten in terms of reader supplied data) added check for empty result in get_sbs_between method changed method get_two_closest_mappings to produce position of looked up sb in returned list, as we'll need it in the future

--- data_structures/chromosome.py
+++ data_structures/chromosome.py
@@ -9,8 +9,9 @@ __status__ = 'develop'
 
 
 class Chromosome(object):
-    def __init__(self, name):
+    def __init__(self, name, genome):
         self.name = name
+        self.genome = genome
         self.start_telomer = StartTelomereSyntenyBlock()
         self.end_telomer = EndTelomereSyntenyBlock()
         self.blocks = AVLTree()
@@ -22,7 +23,17 @@ class Chromosome(object):
         self.mapping = defaultdict(lambda: defaultdict(list))
 
     def add_sb(self, sb):
+        if not type(sb, SyntenyBlock):
+            sb = self._create_sb(sb)
         self.blocks.insert(key=(sb.start, sb.end), value=sb)
+
+    def _create_sb(self, info):
+        sb = SyntenyBlock(name=info.name,
+                          start=info.start,
+                          end=info.end,
+                          strand=info.strand,
+                          chromosome=self)
+        return sb
 
     def add_mapping(self, sb, res_to_map, sbs):
         for synteny_block in sbs:
@@ -40,23 +51,28 @@ class Chromosome(object):
     def get_sbs_between(self, sb_start, sb_end):
         # slice from 1: because, value_slice returns all values, which keys are greater or equals that minimum,
         # but less than maximum. Because of that "or equal" we have to slice
-        return list(self.blocks.value_slice((sb_start.start, sb_start.end), (sb_end.start, sb_end.end)))[1:]
+        result = list(self.blocks.value_slice((sb_start.start, sb_start.end), (sb_end.start, sb_end.end)))
+        return result[1:] if len(result) > 0 else result
 
-    def get_two_closest_mappings(self, sb):
+    def get_two_closest_mappings_and_relative_position(self, sb, res_to_map):
+        position = 0
+        if res_to_map in self.mapping[sb]:
+            return sb, sb, position
         for value in self.blocks.value_slice(start_key=(sb.start, sb.end)):
-            if value in self.mapping:
+            if res_to_map in self.mapping[value]:
                 right_closest_mapping = value
                 break
         else:
             right_closest_mapping = self.end_telomer
         for value in self.blocks.value_slice(end_key=(sb.start, sb.end),
                                              reverse=True):
-            if value in self.mapping:
+            position += 1
+            if res_to_map in self.mapping[value]:
                 left_closest_mapping = value
                 break
         else:
             left_closest_mapping = self.start_telomer
-        return left_closest_mapping, right_closest_mapping
+        return left_closest_mapping, right_closest_mapping, position
 
     def __hash__(self):
         return self.name.__hash__()
